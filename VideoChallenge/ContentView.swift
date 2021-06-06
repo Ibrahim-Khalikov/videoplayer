a9790bae4ce1511a865ea697b3a3edc9b71cf94d
Ibrahim Khalikov <starlink050@gmail.com>
2021-06-06 16:56:33 +0500

    Initial Commit

---
+++ VideoChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoChallenge
+//
+//  Created by cloud_vfx on 06/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoChallenge/VideoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoChallengeApp.swift
+//  VideoChallenge
+//
+//  Created by cloud_vfx on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
